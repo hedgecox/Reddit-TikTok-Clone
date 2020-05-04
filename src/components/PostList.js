810ce413acd36cbdf7b4bfd301bbdd9d84e0ad22
Luke Hedgecox <luke@emotion-robotics.com>
2020-05-04 22:34:46 +0100

    Multiple Requests

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,68 +8,61 @@ import Loading from "./Loading";
 import { getPosts } from "../redux/actions";
 
 const PostContainer = styled.div`
-	scroll-snap-type: y mandatory;
-	overflow-y: scroll;
-	height: ${(props) => props.height}px;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    height: ${(props) => props.height}px;
 `;
 
 const PostList = ({ getPosts, Posts, Sub = "EarthPorn" }) => {
-	const postContainerRef = useRef(null);
-	const [height, setHeight] = useState(window.innerHeight);
+    const postContainerRef = useRef(null);
+    const [height, setHeight] = useState(window.innerHeight);
 
-	useEffect(() => {
-		getPosts(Sub);
+    useEffect(() => {
+        getPosts(Sub);
 
-		const handleResize = debounce(() => {
-			setHeight(window.innerHeight);
-		}, 500);
+        const handleResize = debounce(() => {
+            setHeight(window.innerHeight);
+        }, 500);
 
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, [getPosts, Sub]);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [getPosts, Sub]);
 
-	if (!Posts[Sub]) {
-		return (
-			<PostContainer height={height}>
-				<Loading />
-			</PostContainer>
-		);
-	}
+    if (!Posts[Sub]) {
+        return (
+            <PostContainer height={height}>
+                <Loading />
+            </PostContainer>
+        );
+    }
 
-	const scrollStop = () => {
-		console.log("StopScroll");
+    const scrollStop = () => {
+        const { scrollTop, clientHeight, scrollHeight } = postContainerRef.current;
+        const itemsLeft = scrollHeight / clientHeight - scrollTop / clientHeight;
+        console.log("itemsLeft", itemsLeft);
 
-		const { scrollTopMax, scrollTop, clientHeight, scrollHeight } = postContainerRef.current;
-		console.log("ScrollTopMax", scrollTopMax);
-		console.log("scrollHeight", scrollHeight); //Have to use this instead of scrollTopMax
-		console.log("SCrollTop", scrollTop);
-		console.log("clientHeight:", clientHeight);
-		console.log(postContainerRef.current);
+        if (itemsLeft < 5) {
+            console.log("need more items");
+            getPosts(Sub, Posts[Sub].after);
+        }
+    };
 
-		//console.log("Currently On:", scrollTop / clientHeight);
-
-		const itemsLeft = (scrollTopMax - scrollTop) / clientHeight;
-		if (itemsLeft < 6) {
-			console.log("need more items");
-		}
-	};
-
-	//console.log(items.data.children);
-	return (
-		<PostContainer onScroll={debounce(scrollStop, 120)} ref={postContainerRef} height={height}>
-			{Posts[Sub].children
-				.filter((i) => i.data.post_hint === "image")
-				.map(({ data: { id, title, thumbnail, url, ups, num_comments } }) => {
-					return <Post key={id} title={title} thumbnail={thumbnail} url={url} ups={ups} num_comments={num_comments} />;
-				})}
-		</PostContainer>
-	);
+    //console.log(items.data.children);
+    return (
+        <PostContainer onScroll={debounce(scrollStop, 200)} ref={postContainerRef} height={height}>
+            {Posts[Sub].children
+                .filter((i) => i.data.post_hint === "image")
+                .map(({ data: { id, title, thumbnail, url, ups, num_comments } }) => {
+                    return <Post key={id} title={title} thumbnail={thumbnail} url={url} ups={ups} num_comments={num_comments} />;
+                })}
+        </PostContainer>
+    );
 };
 
 const mapStateToProps = (state) => {
-	return {
-		Posts: state.Posts,
-	};
+    return {
+        Posts: state.Posts,
+    };
 };
 
 export default connect(mapStateToProps, { getPosts })(PostList);
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,12 +1,21 @@
 import { combineReducers } from "redux";
 
 const Posts = (state = {}, action) => {
-	switch (action.type) {
-		case "GOT_POSTS":
-			return { ...state, ...action.payload };
-		default:
-			return state;
-	}
+    switch (action.type) {
+        case "GOT_POSTS":
+            const [key, value] = Object.entries(action.payload)[0];
+
+            //console.log(key);
+            //console.log(value);
+
+            return {
+                ...state,
+                ...{ [key]: { children: [...(state[key]?.children ?? []), ...action.payload[key].children], after: action.payload[key].after } },
+            };
+        //return { ...state, ...action.payload };
+        default:
+            return state;
+    }
 };
 
 export default combineReducers({ Posts });
